6f22a8521f724dbfd90642c08c293604257ed7f6
Jacob Teo <jacobtpl@mit.edu>
2023-05-26 01:18:52 -0400

    final PBDS ref

--- content/data-structures/PBDS.h
+++ content/data-structures/PBDS.h
@@ -1,4 +1,3 @@
-#include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 #include <ext/pb_ds/priority_queue.hpp>
@@ -6,37 +5,38 @@
 using namespace std;
 using namespace __gnu_pbds;
 using namespace __gnu_cxx;
-#define pb push_back
-int main() {
-	tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> t;
-	t.insert(1);
-	t.insert(7);
-	t.insert(2);
-	t.insert(5);
-	assert(*t.find_by_order(2)==5);
-	assert(t.find_by_order(5)==t.end());
-	assert(t.order_of_key(4)==2);
-	assert(t.order_of_key(5)==2);
-	assert(t.order_of_key(8)==4);
+template<class T>
+using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T>
+using Heap = __gnu_pbds::priority_queue<T, less<T>, pairing_heap_tag>;
+//binary_heap_tag, pairing_heap_tag, binomial_heap_tag, rc_binomial_heap_tag, thin_heap_tag
+void pbds() {	
+	Tree<int> t, t2; t.insert(8);
+	auto it = t.insert(10).first;
+	assert(it == t.lower_bound(9));
+	assert(t.order_of_key(10) == 1);
+	assert(t.order_of_key(11) == 2);
+	assert(*t.find_by_order(0) == 8);
+	t.join(t2); // assuming T < T2 or T > T2, merge t2 into t
 
-	__gnu_pbds::priority_queue<int,less<int>,pairing_heap_tag> pq1,pq2;
-	//binary_heap_tag, pairing_heap_tag, binomial_heap_tag, rc_binomial_heap_tag, thin_heap_tag
-	pq1.push(1);
-	pq2.push(5);
-	pq1.join(pq2);
+	Heap<int> pq1, pq2;
+	pq1.push(1); pq2.push(5);
+	pq1.join(pq2); // merge pq2 into pq1
 	assert(pq1.top()==5);
-	auto it=pq1.push(3);
+	auto pq_it = pq1.push(3);
 	assert(pq1.top()==5);
-	pq1.modify(it,7);
+	pq1.modify(pq_it,7); // modify-key in O(log N)
 	assert(pq1.top()==7);
 	
-	int n=5;
-    rope <int> v(n, 0);
-    for(int i = 0; i < n; ++i) v.mutable_reference_at(i) = i + 1; //push_back
+	int n=3;
+    rope<int> v(n, 0);
+    for (int i=0; i<n; i++) v.mutable_reference_at(i) = i + 1; // (1 2 3)
+	for (int i=0; i<n; i++) v.push_back(i + n + 1); // (1 2 3 4 5 6)
     int l=1, r=3;
-    rope <int> cur = v.substr(l, r-l+1);
-    v.erase(l, r-l+1);
-    v.insert(v.mutable_begin(), cur); //to start
-    v.insert(v.mutable_reference_at(0), cur); //to one after start
-    for (int x:v) printf("%d\n", x);
+    rope<int> cur = v.substr(l, r-l+1); // 2 3 4
+    v.erase(l, r-l+1); // 1 5 6
+	v.insert(v.mutable_begin() + 2, cur);
+    v.insert(v.mutable_begin(), cur); //to start (2 3 4 1 5 6)
+    // v.insert(v.mutable_reference_at(0), cur); // to ONE AFTER start (1 2 3 4 5 6)
+    // v.insert(v.mutable_begin() + 2, cur); // to TWO AFTER start (1 5 2 3 4 6)
 }
