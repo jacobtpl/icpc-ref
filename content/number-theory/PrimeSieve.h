765035048415dc182b614c4c69276ca7d0920f50
Jacob Teo <jacobtpl@mit.edu>
2023-05-27 23:58:02 -0400

    add siyong sieve and final ref

---
+++ content/number-theory/PrimeSieve.h
@@ -0,0 +1,39 @@
+/**
+ * Author: Siyong
+ * Date: 2023
+ * License: CC0
+ * Description: Prime sieve but slow, for generating all primes smaller than LIM.
+ * Time: LIM=1e9 $\approx$ 8.5s
+ * Status: kinda tested
+ */
+int const LIM = 1e7+5;
+std::vector<bool> cp;
+std::vector<int> pr, nx, lp, cnt;
+void sieve()
+{
+	cp.assign(LIM, 0), nx.assign(LIM, -1), lp.assign(LIM, -1), cnt.assign(LIM, -1);
+	for(int i=2;i<LIM;++i) {
+		if(!cp[i])
+			lp[i] = pr.size(), nx[i] = cnt[i] = 1, pr.push_back(i);
+		for(int j=0,k;j<pr.size() && (k=i*pr[j])<LIM;++j) { // overflow warning?
+			cp[k] = 1, lp[k] = j;
+			if(j == lp[i]) {
+				nx[k] = nx[i], cnt[k] = cnt[i]+1; break;
+			} else nx[k] = i, cnt[k] = 1;;
+		}
+	}
+}
+/*
+int main() {
+	sieve();
+	int N; scanf("%d", &N);
+	for(int i=0;i<N;++i) {
+		int x;
+		scanf("%d", &x);
+		for(;x>1;x=nx[x])
+			for(int i=0;i<cnt[x];++i)
+				printf("%d ", pr[lp[x]]);
+		printf("\n");
+	}
+	return 0;
+}*/
--- content/strings/SuffixAutomaton.h
+++ content/strings/SuffixAutomaton.h
@@ -7,32 +7,23 @@
  * Usage: just do it
  * Status: not super optimized
  */
-#include <vector>
-#include <map>
-struct SA
-{
-public:
+struct SA {
 	std::vector<std::map<char, int> > adj;
 	std::vector<int> link, dis;
 	int N;
 	SA(): adj(1), link(1, -1), dis(1, 0), N(1) {}
-
-	int new_node(int v=-1)
-	{
+	int new_node(int v=-1) {
 		if(v == -1)
 			adj.emplace_back(), link.emplace_back(), dis.emplace_back();
 		else
 			adj.push_back(adj[v]), link.push_back(link[v]), dis.push_back(dis[v]);
 		return N++;
 	}
-
-	int go(int p, int c)
-	{
+	int go(int p, int c) {
 		auto it = adj[p].find(c);
 		if(dis[it->second] == dis[p] + 1)
 			return it->second;
-		else
-		{
+		else {
 			int q = it->second, n = new_node(q);
 			dis[n] = dis[p] + 1, link[q] = n;
 			for(;p != -1 && (it = adj[p].find(c))->second == q;p = link[p])
@@ -40,13 +31,10 @@ public:
 			return n;
 		}
 	}
-
-	int append(int p, char c)
-	{
+	int append(int p, char c) {
 		auto it = adj[p].find(c);
 		if(it != adj[p].end())
 			return go(p, c);
-
 		int n = new_node();
 		dis[n] = dis[p] + 1;
 		for(;p != -1 && adj[p].find(c) == adj[p].end();p = link[p])
@@ -57,9 +45,7 @@ public:
 			link[n] = go(p, c);
 		return n;
 	}
-
-	int add(std::string const &s)
-	{
+	int add(std::string const &s) {
 		int n = 0;
 		for(char c: s)
 			n = append(n, c);
