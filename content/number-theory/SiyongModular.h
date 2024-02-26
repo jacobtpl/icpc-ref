cac981b02522b28376d1e07dc807fb0690a9403f
Jacob Teo <jacobtpl@mit.edu>
2024-02-26 17:41:18 -0500

    update siyong mod

--- content/number-theory/SiyongModular.h
+++ content/number-theory/SiyongModular.h
@@ -2,36 +2,36 @@ using ll = long long;
 
 int const MOD = 998244353;
 
-struct mint
-{
-public:
+ll euclid(ll a, ll b, ll &x, ll &y) {
+	if (!b) return x = 1, y = 0, a;
+	ll d = euclid(b, a % b, y, x);
+	return y -= a/b * x, d;
+}
+
+struct mint {
 	int v;
-
 	explicit operator int() {return v;}
-
 	mint(): v(0) {}
-	template<int> mint(int _v): v(_v)
-	{
-		if(v < -MOD || MOD <= v)
-			v %= MOD;
-		if(v < 0) v += MOD;
+	mint(auto z): {
+		z %= MOD;
+		if (z < 0) z += MOD;
+		v = z;
 	}
-	template<typename T> mint(T _v)
-	{
-		v = _v % MOD;
-		if(v < 0) v += MOD;
+	mint invert(mint a) {
+		ll x, y, g = euclid(a.v, MOD, x, y);
+		assert(g == 1); return mint((x + MOD) % MOD);
 	}
-
 	mint& operator+= (mint const& o) {if((v+=o.v)>=MOD) v-=MOD; return *this;}
 	mint& operator-= (mint const& o) {if((v-=o.v)<0) v+=MOD; return *this;}
 	mint& operator*= (mint const& o) {v=(ll)v*o.v%MOD; return *this;}
+	mint& operator/= (mint const& o) {return *this *= invert(o);}
 
 	friend mint operator+ (mint a, mint const& b) {return a+=b;}
 	friend mint operator- (mint a, mint const& b) {return a-=b;}
 	friend mint operator* (mint a, mint const& b) {return a*=b;}
+	friend mint operator/ (mint const& a, mint const& b) {return a*invert(b);}
 
-	template<typename T> friend mint pow(mint a, T b)
-	{
+	friend mint pow(mint a, auto b) {
 		mint r(1);
 		for(;b;b>>=1, a*=a)
 			if(b&1)
