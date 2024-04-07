c821d6171438f43a9858049680eafb59b0fe93c2
Siyong Huang <huangsy222@gmail.com>
2024-04-06 22:29:07 -0400

    Centroid code

--- content/graph/Centroid.h
+++ content/graph/Centroid.h
@@ -1,7 +1,18 @@
+/**
+ * Author: Siyong
+ * Date: April 6, 2024
+ * Description: Boilerplate centroid decomp code
+ * Status: Not tested
+ * Time: O(N\log N)
+ */
+
 struct Centroid {
 	vector<vi> const& adj;
 	int N;
 	vector<int> s, rem;
+	vector<vi> links;
+	vector<pair<int, int> > par; // <parent centroid, index>
+	int root;
 	int dfs(int n, int p=-1) {
 		s[n]=1;
 		for(int x: adj[n]) if(x!=p && !rem[x])
@@ -14,14 +25,17 @@ struct Centroid {
 			{p=n,n=x,k=1; break;}
 		return n;
 	}
-	void cent(int start=0) {
+	int cent(int start=0) {
 		int c = find(start, dfs(start));
 		// Do stuff with c. Just remember to check both (x != p && !rem[x])
 		rem[c]=1;
 		for(int x:adj[c]) if(!rem[x])
-			cent(x);
+		{
+			int v = cent(x);
+			par[v] = {c, sz(links[c])};
+			links[c].push_back(v);
+		}
+		return c;
 	}
-	Centroid(vector<vi> const& adj): adj(adj), N(adj.size()), s(N), rem(N) {
-		cent();
-	}
+	Centroid(vector<vi> const& adj): adj(adj), N(adj.size()), s(N), rem(N), links(N), par(N,{-1,-1}), root(cent()) {}
 };
