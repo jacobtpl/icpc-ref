b2609f39c9f5c015192d009031763f68dd9b023a
vthomasa <97055602+vthomasa@users.noreply.github.com>
2024-04-10 15:49:40 -0400

    Update template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define rep(i,a,b) for(int i = (a); i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+#define mp make_pair
+#define pb push_back
+using ll = long long;
+using pii = pair<int,int>;
+using vi = vector<int>;
 
 bool ckmax(auto &a, auto const& b) {return b>a?a=b,1:0;}
 bool ckmin(auto &a, auto const& b) {return b<a?a=b,1:0;}
 
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
-}
+int main() { cin.tie(0);cin.sync_with_stdio(0); }
